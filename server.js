54d52c230030c04676f0df60192ea461b84a6bd4
jvzin2k09 <joaovictorramoscoelho143@gmail.com>
2026-05-25 09:23:28 -0300

    continuando

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require ('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+mongoose.connection.on('connected', () => {
+    console.log('conectado com o MongoDB');
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
